3f57976711eea7308621d4b060b828b76db8c8dc
chyyyy15 <131245872+chyyyy15@users.noreply.github.com>
2023-04-23 16:43:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -75,6 +75,9 @@ def handle_message(event):
         )
     )
         line_bot_api.reply_message(event.reply_token, buttons_template_message)
+    else:
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
+        
     if re.match('服務條款',message):
         confirm_template_message = TemplateSendMessage(
             alt_text='服務條款',
