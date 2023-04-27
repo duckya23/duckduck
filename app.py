18a95a8211a4e15c89158724396a3d5210c2193d
Kuooooooo <113331852+Kuooooooo@users.noreply.github.com>
2023-04-27 19:54:36 +0800

    Update app.py

--- app.py
+++ app.py
@@ -43,25 +43,26 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = text=event.message.text
-   if re.match('其他功能',message):
+   if re.match('告訴我秘密',message):
         buttons_template_message = TemplateSendMessage(
-        alt_text='其他功能來了鴨',
+        alt_text='這個看不到',
         template=ButtonsTemplate(
-            thumbnail_image_url='https://png.pngtree.com/png-vector/20190831/ourlarge/pngtree-gear-vector-icon-template-png-image_1717225.jpg',
-            title='其他功能鴨！',
-            text='記帳鴨博士的其他功能',
+            thumbnail_image_url='https://i.imgur.com/wpM584d.jpg',
+            title='行銷搬進大程式',
+            text='選單功能－TemplateSendMessage',
             actions=[
-               MessageAction(
-                    label='新手教學',
-                    text='新手教學'
+                PostbackAction(
+                    label='偷偷傳資料',
+                    display_text='檯面上',
+                    data='action=檯面下'
                 ),
                 MessageAction(
-                    label='服務條款',
-                    text='服務條款'
+                    label='光明正大傳資料',
+                    text='我就是資料'
                 ),
-                MessageAction(
-                    label='Line Pay 連結',
-                    text='Line Pay 連結'
+                URIAction(
+                    label='行銷搬進大程式',
+                    uri='https://marketingliveincode.com/'
                 )
             ]
         )
