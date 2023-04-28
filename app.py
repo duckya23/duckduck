8a2acc67a66d96c72570526d89845f5c60cb1715
o172o <131576629+o172o@users.noreply.github.com>
2023-04-28 13:34:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -40,9 +40,7 @@ def callback():
 
 #訊息傳遞區塊
 ##### 基本上程式編輯都在這個function #####
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    message = text=event.message.text    
+  
     if re.match('其它功能',message):
         buttons_template_message = TemplateSendMessage(
         alt_text='其它功能來了鴨',
