7a5d914350c7492c998ac3b3fb1bc4e552f989d7
yuhsaun <113332368+yuhsaun@users.noreply.github.com>
2023-04-29 21:59:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -146,7 +146,7 @@ def handle_message(event):
         
       
     elif re.match('飲食預算',message):
-        line_bot_api.reply_message(event.reply_token,TextSendMessage('鴨鴨！你要設定多少飲食預算鴨！'))    
+        line_bot_api.reply_message(event.reply_token,TextSendMessage('鴨鴨！你要設定多少飲食預算鴨！'))           
     elif re.match('日用預算',message):
         line_bot_api.reply_message(event.reply_token,TextSendMessage('鴨鴨！你要設定多少日用預算鴨!')) 
     elif re.match('居家預算',message):
