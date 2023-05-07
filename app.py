d95585afaad51357bbd5021f8deac34771140563
o172o <131576629+o172o@users.noreply.github.com>
2023-05-07 18:33:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -186,11 +186,13 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token, flex_message)
     elif re.match('飲食',message):
         line_bot_api.reply_message(event.reply_token,TextSendMessage('請輸入餐飲金額鴨!'))
-        amount =event.message.text
-        if re.match('我的金額',amount):
-            line_bot_api.reply_message(event.reply_token,TextSendMessage('金額為多少'))
-        else:
-            line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
+        @handler.add(MessageEvent, message=TextMessage)
+        def handle_message(event):
+            amount =event.message.text
+            if re.match('我的金額',amount):
+                line_bot_api.reply_message(event.reply_token,TextSendMessage('金額為多少'))
+            else:
+                line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
             
     elif re.match('交通',message):
         line_bot_api.reply_message(event.reply_token,TextSendMessage('請輸入交通金額鴨!')) 
