1153fa5f6b1c04d7bf992981ad02d860c8ef67ed
Kuooooooo <113331852+Kuooooooo@users.noreply.github.com>
2023-05-11 17:50:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -58,9 +58,9 @@ def handle_message(event):
             title='其它功能鴨！',
             text='記帳鴨博士的其它功能',
             actions=[
-                URIAction(
+                MessageAction(
                     label='使用說明',
-                    uri='https://sites.google.com/view/ducktermserver/%E9%A6%96%E9%A0%81'
+                    print("嗨")
                 ),
                 URIAction(
                     label='服務條款',
