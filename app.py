da6ea43d7bb41e8401c2784b6d9000ab52a1aa5e
yuhsaun <113332368+yuhsaun@users.noreply.github.com>
2023-05-11 19:00:50 +0800

    app.py

--- app.py
+++ app.py
@@ -167,7 +167,9 @@ def handle_message9(event):
         
       
     elif re.match('飲食預算',message):
-        line_bot_api.reply_message(event.reply_token,TextSendMessage('鴨鴨！你要設定多少飲食預算鴨！')) 
+        line_bot_api.reply_message(event.reply_token,TextSendMessage('鴨鴨！你要設定多少飲食預算鴨！'))
+    elif re.match('3000',message):
+        line_bot_api.reply_message(event.reply_token,TextSendMessage('已經幫您儲存了鴨！'))
     elif re.match('2000',message):
         line_bot_api.reply_message(event.reply_token,TextSendMessage('幫你儲存了鴨！'))
     elif re.match('日用預算',message):
