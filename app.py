a1908ad8c14054db3b96b8f5a27bdb6f1cd085cc
Kuooooooo <113331852+Kuooooooo@users.noreply.github.com>
2023-05-26 13:53:44 +0800

    Update app.py

--- app.py
+++ app.py
@@ -173,12 +173,6 @@ def handle_message9(event):
             )
         )
         line_bot_api.reply_message(event.reply_token, image_carousel_template_message)
-    elif re.match('你們是誰鴨？？',message):
-        line_bot_api.reply_message(event.reply_token,TextSendMessage('沒錯喔就是鴨，我們是鴨鴨團隊喔！'))
-    elif re.match('講個笑話來聽聽鴨',message):
-        line_bot_api.reply_message(event.reply_token,TextSendMessage('鴨子搭計程車，猜一種蔬果?答案是小黃瓜！(小黃，呱！)'))
-    elif re.match('那根是什麼鴨？',message):
-        line_bot_api.reply_message(event.reply_token,TextSendMessage('所以說為什麼大家不拿著熱狗一起拍照呢？哎，哭笑不得鴨:)'))  
         
 #     elif re.match('LinePay收據',message):
 #         flex_message = FlexSendMessage(
@@ -537,6 +531,16 @@ def handle_message9(event):
         line_bot_api.reply_message(event.reply_token, TextSendMessage(reply_text))
     elif re.match('我把禾鴨加入購物車囉',message):
         line_bot_api.reply_message(event.reply_token,TextSendMessage('你好棒鴨'))
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(reply_text))
+    elif re.match('你們是誰鴨？？',message):
+        line_bot_api.reply_message(event.reply_token,TextSendMessage('沒錯喔就是鴨，我們是鴨鴨團隊喔！'))
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(reply_text))
+    elif re.match('講個笑話來聽聽鴨',message):
+        line_bot_api.reply_message(event.reply_token,TextSendMessage('鴨子搭計程車，猜一種蔬果?答案是小黃瓜！(小黃，呱！)'))
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(reply_text))
+    elif re.match('那根是什麼鴨？',message):
+        line_bot_api.reply_message(event.reply_token,TextSendMessage('所以說為什麼大家不拿著熱狗一起拍照呢？哎，哭笑不得鴨:)'))  
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(reply_text))
         
     elif re.match('月支出占比圓餅圖',message):
         image_message = ImageSendMessage(
@@ -551,9 +555,7 @@ def handle_message9(event):
             preview_image_url='https://i.imgur.com/pT1Fm9d.png'
         )
         line_bot_api.reply_message(event.reply_token, image_message)
-    elif re.match('講個笑話來聽聽鴨',message):
-        line_bot_api.reply_message(event.reply_token,TextSendMessage('答案是小黃瓜!(小黃，呱!)'))    
-     
+
     else:
         line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
         
