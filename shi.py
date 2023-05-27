09da2d28fb7d543423df7883029dde2b70f8b056
o172o <131576629+o172o@users.noreply.github.com>
2023-05-27 17:47:28 +0800

    Update shi.py

--- shi.py
+++ shi.py
@@ -1,24 +1,3 @@
-# -*- coding: utf-8 -*-
-#載入LineBot所需要的套件
-from flask import Flask, request, abort
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
-from linebot.models import *
-import re
-
-
-app = Flask(__name__)
-# 必須放上自己的Channel Access Token
-line_bot_api = LineBotApi('cA4Y+naWER+/PyaAYGacJOO6GznxzUz/vUHhFzmY2eVvIczqzh7InjUL9h2IPo50NOyMXbbZXrNUYySlyd2CXHtlKYTDlkhIdGitB/ZKTvULCJnDf4PoVKyHgZRp7FIuHZcLErYwgQ2/Xd1ZRiFr4QdB04t89/1O/w1cDnyilFU=')
-# 必須放上自己的Channel Secret
-handler = WebhookHandler('05a15941e79753db37de1062d1cec8dd')
-#line_bot_api.push_message('你自己的ID', TextSendMessage(text='你可以開始了'))
-
-
 def is_number(input_str):
     try:
         float(input_str)  # 嘗試將輸入轉換為浮點數
@@ -29,12 +8,34 @@ def is_number(input_str):
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message2():
     message = text=event.message.text
+    with open('Account_number.csv', 'r') as file:
+    Account_number =int( file.read())
+
     if is_number(message): # 當message是數字，執行這個
         with open('food.csv', 'r') as file: # 讀取food金額
             food =int( file.read())            
         food +=float(message) 
         with open('food.csv', 'w+') as file: # 寫入food金額
             file.write(str(food))
+        Account_number+=1
+        import time
+        localtime = time.asctime( time.localtime(time.time()) )
+        import csv
+        my_dict = {}
+        filename = 'my_dict.csv'
+        # 開啟 CSV 文件並追加字典內容
+        with open(filename, 'a', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            # 檢查是否為空檔案，若是則寫入標題行
+            if csvfile.tell() == 0:
+                writer.writerow(['Key', 'Value1', 'Value2'])
+            key = 'my_key23'
+            value1 = localtime
+            value2 = float(message)
+            my_dict[key] = [value1, value2]  # 使用鍵作為索引，將兩個值分配給該鍵
+            # 寫入數據行
+            for key, values in my_dict.items():
+                writer.writerow([key] + values)
         reply_text = f'已儲存金額 {a} 鴨！'  # 建立回覆訊息
         line_bot_api.reply_message(event.reply_token, TextSendMessage(reply_text))
     else : # 當message不是數字，執行這個
