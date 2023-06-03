3dfa2e507245f2d4f854d2d39f496029f37970a6
o172o <131576629+o172o@users.noreply.github.com>
2023-06-03 11:03:48 +0800

    Update apptest.py

--- apptest.py
+++ apptest.py
@@ -18,6 +18,10 @@ line_bot_api = LineBotApi('cA4Y+naWER+/PyaAYGacJOO6GznxzUz/vUHhFzmY2eVvIczqzh7In
 handler = WebhookHandler('05a15941e79753db37de1062d1cec8dd')
 #line_bot_api.push_message('你自己的ID', TextSendMessage(text='你可以開始了'))
 
+line_bot_api.push_message(443dipnx, 
+                          TextSendMessage(text='安安您好！早餐吃了嗎？'))
+
+
 
 #以下這段目前沒有作用，可刪除-目前只是裝飾用，因為呼叫不捯這個函數
 @handler.add(MessageEvent, message=TextMessage)
